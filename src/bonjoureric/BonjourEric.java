4eed35894fc6fdce2c3f87be646c2c9153577b01
EricMasson <eric.division@gmail.com>
2023-05-11 22:47:37 -0400

    premier commit

---
+++ src/bonjoureric/BonjourEric.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bonjoureric;
+
+/**
+ *
+ * @author ericd
+ */
+public class BonjourEric {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour " + args[0]+", "+args[1]) ;
+    }
+    
+}
